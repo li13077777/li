21a80af67d9bd427444a3bc7bf8b2153c342c3a8
li13077777 <103730466+li13077777@users.noreply.github.com>
2022-10-02 00:03:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_count():
   return delta.days
 
 def get_birthday():
-  next = datetime(str(date.today().year) + "-" + birthday,"%Y-%d-%m")
+  next = datetime((date.today().year) + "-" + birthday,"%Y-%d-%m")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
